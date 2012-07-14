cd09a47e643783b17eb255473cf1d48bc971b852
Brian Minton <brian@minton.name>
2012-07-14 00:51:24 -0400

    added signal handlers, fixed input buffer reading bug

--- src/lifter.c
+++ src/lifter.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
 typedef struct {
     int x;
@@ -28,10 +30,11 @@ world read_map()
         line[strchr(line,'\n')-line]='\0';
         buf=realloc(buf,(1+line_no) * sizeof (char*));
         buf[line_no]=calloc(strlen(line),sizeof (char*));
-	strcpy(buf[line_no],line);
+        strcpy(buf[line_no],line);
         line_no++;
-	if (max_len < strlen(line))
-		max_len=strlen(line);
+        if (max_len < strlen(line)) {
+            max_len=strlen(line);
+        }
     }
 
     w.buf=buf;
@@ -40,10 +43,43 @@ world read_map()
     return w;
 }
 
+int calc_abort_score() {
+    fprintf(stderr,"FIXME: calc_abort_score()\n");
+    return 1;
+}
+
+void last_second(){
+    /* if score is positive (or would be with the bonus), send Abort command */
+    fprintf(stderr,"checking for last second abort\n");
+    if (calc_abort_score() > 0) {
+        putchar('A');
+        fflush(stdout);
+    }
+}
+
+void sig_handler(int signum)
+{
+    if (signum==SIGINT){
+        fprintf(stderr,"caught SIGINT\n");
+        alarm(9);
+    } else if (signum==SIGALRM) {
+        fprintf(stderr,"caught SIGALRM\n");
+        last_second();
+    }
+}
+
 int main()
 {
-    world w;
-    w=read_map();
+    world w=read_map();
+    int x=0;
+
+    signal(SIGINT,sig_handler);
+    signal(SIGALRM,sig_handler);
+
+
+    while (1){
+        x++;
+    }
 
     fprintf(stderr,"x: %d y: %d\n",w.x_size,w.y_size);
 
