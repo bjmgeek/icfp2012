f864ac9a93e726bceb1b6f2e894e6a6489db823d
Brian Minton <brian@minton.name>
2012-07-15 14:25:04 -0400

    trying to use wait, and running the sleeps in the background

--- run.sh
+++ run.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
-
-function trapped {
-	echo got SIGCHLD $0
-	exit
-}
-
-
-trap 'trapped' SIGCHLD
-
 ./lifter < $1 &
 pid=$!
-sleep 150
-echo 150 seconds elapsed, sending SIGINT
-kill -INT $pid
-sleep 10
-echo 160 seconds elapsed, sending SIGINT
-kill -KILL $pid
-exit
+
+( sleep 150 ; kill -INT $pid; sleep 10 kill -KILL $pid ) &
+wait $pid
